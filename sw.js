1a21ea8feda1c2411dd670994af4081ea7240d84
luizmourademoura <78830766+luizmourademoura@users.noreply.github.com>
2025-05-05 16:42:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,14 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
+  "./icon_72x72.png",
+  "./icon_96x96.png",
+  "./icon_128x128.png",
+  "./icon_144x144.png",
+  "./icon_152x152.png",
+  "./icon_192x192.png",
+  "./icon_384x384.png",
+  "./icon_512x512.png",
   "./verdemalaquita.html",
   "./mebendazol.html",
   "./niclosamida.html",
@@ -31,7 +37,39 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+  // Estratégia: Cache primeiro para arquivos estáticos (HTML, JS, CSS e ícones)
+  if (event.request.method === "GET" && (event.request.url.endsWith(".html") || event.request.url.endsWith(".png") || event.request.url.endsWith(".css"))) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            // Armazenar a resposta da rede no cache para futuras requisições
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+    );
+  } else {
+    // Para outras requisições (como JS, API, etc.), faz-se o fetch diretamente
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
+});
+
+// Evento de ativação para limpar caches antigos, se necessário
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
